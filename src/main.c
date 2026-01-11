388b36657ad951d098979a9386a87512d1b21015
babidrummond <babidrummond@hotmail.com>
2026-01-11 17:10:52 -0300

    added libft as submodule, started coding

--- src/main.c
+++ src/main.c
@@ -6,11 +6,22 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 16:58:07 by bmoreira          #+#    #+#             */
-/*   Updated: 2026/01/11 16:58:28 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/01/11 17:08:30 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "pipex.h"
+
+void	validate_args(int count, char **args)
 {
+	if (count != 5)
+		ft_printf("Invalid arguments. "
+			"Usage: ./pipex [file1] [cmd1] [cmd2] [file2]\n");
+	(void) args;
+}
+
+int	main(int argc, char **argv)
+{
+	validate_args(argc, argv);
 	return (0);
 }
